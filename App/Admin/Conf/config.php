c7964768dedd69b2883ee02c82f64f5b65592e67
qyk <2710166421@qq.com>
2018-07-16 16:02:54 +0800

    应用配置和模式配置

--- App/Admin/Conf/config.php
+++ App/Admin/Conf/config.php
@@ -1,8 +1,8 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-	'TMPL_PARSE_STRING'  =>array(     
-	'__STATIC__' => SITE_URL . '/App/Admin/static',
-	'__UPLOAD__' => '/Uploads', // 增加新的上传路径替换规则
+	'TMPL_PARSE_STRING'  =>array(  
+		'__STATIC__' => SITE_URL . '/App/Admin/static',
+		'__UPLOAD__' => '/Uploads', // 增加新的上传路径替换规则
 	)
 );
