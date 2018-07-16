a1bbeb53ceee285346a21b49c9b0452834091850
qyk <2710166421@qq.com>
2018-07-17 00:10:10 +0800

    展示用户列表页面

--- App/Admin/Controller/IndexController.class.php
+++ App/Admin/Controller/IndexController.class.php
@@ -4,7 +4,7 @@ use Think\Controller;
 class IndexController extends Controller {
     public function index(){
     	// 如果没有登录，重定向到登录页面
-    	!session('?admin_id') && $this->redirect('./login');
+    	//!session('?admin_id') && $this->redirect('./login');
 
     	// 展示后台管理首页
         $this->display();
