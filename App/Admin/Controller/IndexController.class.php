6d12fab998dad4079b151a32ceee1ab1ca7c8485
qyk <2710166421@qq.com>
2018-07-16 13:49:09 +0800

    删除运行时的文件，不需要git

--- App/Admin/Controller/IndexController.class.php
+++ App/Admin/Controller/IndexController.class.php
@@ -3,6 +3,8 @@ namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+    	/* 如果没有登录，重定向到登录页面 */
+    	!session('?admin_id') && $this->redirect('./login');
         $this->display();
     }
 }
