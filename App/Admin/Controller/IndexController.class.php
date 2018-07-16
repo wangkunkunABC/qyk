7833728f66567d79630e866ecf0a2b656abb64c8
qyk <2710166421@qq.com>
2018-07-16 13:54:03 +0800

    配置忽略上传文件目录RunTime&后台首页如果没有登录重定向到登陆页

--- App/Admin/Controller/IndexController.class.php
+++ App/Admin/Controller/IndexController.class.php
@@ -3,8 +3,10 @@ namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-    	/* 如果没有登录，重定向到登录页面 */
+    	// 如果没有登录，重定向到登录页面
     	!session('?admin_id') && $this->redirect('./login');
+
+    	// 展示后台管理首页
         $this->display();
     }
 }
